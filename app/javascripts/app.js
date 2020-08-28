064adf8bb318c82a7db711457dfdd7ad8c4f62f8
prabhatcse46 <prabhatcse46@gmail.com>
2020-08-28 00:44:16 -0400

    Update app.js

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,23 +1,21 @@
-// Import the page's CSS. Webpack will know what to do with it.
+
 import "../stylesheets/app.css";
 
-// Import libraries we need.
+
 import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract'
 
-// Import our contract artifacts and turn them into usable abstractions.
+
 import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
 import certificate_artifacts from '../../build/contracts/Certificate.json'
 import certificatesCollection_artifacts from '../../build/contracts/Certificates.json'
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
+
 var MetaCoin = contract(metacoin_artifacts);
 var CertificatesCollection = contract(certificatesCollection_artifacts);
 var Certificate = contract(certificate_artifacts);
 var certificatesCollectionContractAddress ='0x57e223ccb8fb7273f92e7bb83b7a6d5b78b55f48';
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
+
 var accounts;
 var account;
 
@@ -200,7 +198,7 @@ sign: function() {
     var email = $("#_email").val();
     var company = $("#_company").val();
 
-    //function setApprover(string _name, string _email, string _company,string _designation, address _approverAddress ) 
+    
     return instance.setApprover(name,email,company,designation,issuerAddress);
   }).then(function(value) {
     console.log('Approver set');
@@ -243,7 +241,7 @@ getApprover: function() {
     }).then(function() {
       
       self.setStatus("Transaction complete! ");
-      //cert.getOwner();
+      
     }).catch(function(e) {
       console.log(e);
       self.setStatus("Error sending coin; see log.");
@@ -252,14 +250,13 @@ getApprover: function() {
 };
 
 window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+ 
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-    // Use Mist/MetaMask's provider
+    console.warn("Using web3 detected from external source")
+   
     window.web3 = new Web3(web3.currentProvider);
   } else {
-    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    console.warn("No web3 detected. Falling back to http://localhost:8545.");
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
 
